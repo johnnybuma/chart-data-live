c07ebee8649f28e44bb63ef025ada11f6f2f966e
Jonathan Buma <johnny.buma@gmail.com>
2015-09-26 21:52:25 +0000

    HTML to bootstrap convention, style fixes

--- public/js/api.js
+++ public/js/api.js
@@ -54,7 +54,7 @@ options.series = [];
         // remove resultset if this has already been run
         $('.content ul').remove();
         // add spinner to indicate something is happening
-        $('<i class="fa fa-cog fa-1 fa-spin"/>').prependTo('#spinning');
+        $('<i class="fa fa-cog fa-1 fa-spin" style="float: left;"/>').prependTo('#spinning');
         
         // get selected zip code from selectbox
          state = $('#states option:selected').val();
